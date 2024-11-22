0fe65a9a5b2d299f92e5615d15d9b555442df5f0
eoghan523 <eoghan523@gmail.com>
2024-11-22 23:08:03 +0000

    Adde basic homepage.js file and some text.

--- my-first-react-app/src/page/header.js
+++ my-first-react-app/src/page/header.js
@@ -1,1 +1,18 @@
 import react from 'react';
+
+
+const Header = () => {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">Projects</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
---
+++ my-first-react-app/src/page/homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+      <div>
+        <h1>Welcome to My React app!</h1>
+        <p>My name is Eoghan. I am a new developer learning react framework. </p>
+        <p>Please check out my projects below!</p>
+      </div>
+    );
+  };
+  
+  export default Homepage;
